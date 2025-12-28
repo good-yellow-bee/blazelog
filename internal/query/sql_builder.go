36e1ac3317bd744f2f7b0580dc07e0ffbd343654
Piotr Francuz <pfrancuz1@gmail.com>
2025-12-28 20:25:53 +0100

    Fix staticcheck QF1001 + remove review doc

--- internal/query/sql_builder.go
+++ internal/query/sql_builder.go
@@ -391,8 +391,8 @@ func isValidJSONPropertyName(name string) bool {
 		return false
 	}
 	for _, r := range name {
-		if !((r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') ||
-			(r >= '0' && r <= '9') || r == '_' || r == '-') {
+		if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') &&
+			(r < '0' || r > '9') && r != '_' && r != '-' {
 			return false
 		}
 	}
